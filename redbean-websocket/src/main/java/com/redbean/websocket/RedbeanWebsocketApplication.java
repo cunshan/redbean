c57bf22a75a1af58e433838feeac1884a2b144da
王存山 <18661877367@163.com>
2017-09-25 14:19:36 +0800

    初始化

---
+++ redbean-websocket/src/main/java/com/redbean/websocket/RedbeanWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.redbean.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbeanWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedbeanWebsocketApplication.class, args);
+	}
+}
