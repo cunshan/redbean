c64b8b0abc9e45162bf59e6a699553bf17917dc6
王存山 <18661877367@163.com>
2017-09-25 21:29:49 +0800

    websocket广播

--- redbean-websocket/src/main/java/com/redbean/websocket/controller/BroadcastController.java
+++ redbean-websocket/src/main/java/com/redbean/websocket/controller/BroadcastController.java
@@ -6,17 +6,32 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**.
+ * websocket广播实例
+ */
 @Slf4j
 @Controller
-public class GreetingController {
+@RequestMapping("/broadcast")
+public class BroadcastController {
 
   @MessageMapping("/hello")
   @SendTo("/topic/greetings")
   public Greeting greeting(HelloMessage message) {
-
     return new Greeting("Hello " + message.getName() + "!");
   }
 
+
+  @GetMapping("/index")
+  public String index() {
+    return "broadcast/index";
+  }
+
+  @GetMapping("/user")
+  public String user() {
+    return "broadcast/user";
+  }
 
 }
