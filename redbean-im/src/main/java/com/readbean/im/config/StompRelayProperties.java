a2bff532ed635a8f61da049dd16296869fa64235
王存山 <18661877367@163.com>
2017-10-26 10:33:31 +0800

    即时通讯项目初始化

---
+++ redbean-im/src/main/java/com/readbean/im/config/StompRelayProperties.java
@@ -0,0 +1,22 @@
+package com.readbean.im.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "websocket.stomp")
+public class StompRelayProperties {
+
+  private String relayHost = "127.0.0.1";//代理主机地址
+  private Integer relayPort = 61613;//代理主机端口
+  private String clientLogin = "guest";
+  private String clientPasscode = "guest";
+  private String systemLogin = "guest";
+  private String systemPasscode = "guest";
+  private Long systemHeartbeatSendInterval;
+  private Long systemHeartbeatReceiveInterval;
+  private String virtualHost;
+  private Boolean autoStartUp;
+  private String userDestinationBroadcast;
+
+}
